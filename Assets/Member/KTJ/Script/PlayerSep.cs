9e2a7e966265350cbc6fbec58afff0a72351019d
Bimtaeur30 <Bimtaeur30@gmail.com>
2025-09-25 18:43:38 +0900

    First

---
+++ Assets/Member/KTJ/Script/PlayerSep.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerSep : MonoBehaviour
+{
+    [SerializeField] private GameObject BodyPref;
+    
+
+    public void SeparatePlayer()
+    {
+
+    }
+
+    public void CombinePlayer()
+    {
+
+    }
+}
