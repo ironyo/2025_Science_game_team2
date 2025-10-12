622ca9482d8ea0aea660d22f1765821509c9dde8
ironyo <kyou09kang@gmail.com>
2025-10-13 08:49:24 +0900

    vbnvhgytr5e4w32

--- Assets/Member/KU/02.Script/QuizManager.cs
+++ Assets/Member/KU/02.Script/QuizManager.cs
@@ -1,27 +1,70 @@
 using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
+using DG.Tweening;
 
 public class QuizManager : MonoBehaviour
 {
     [SerializeField] List<QuizSO> quizSOs = new List<QuizSO>();
     [SerializeField] List<TextMeshProUGUI> quizTexs = new List<TextMeshProUGUI>();
+    [SerializeField] GameObject quizGroup;
+    [SerializeField] TextMeshProUGUI wrongTex;
+    [SerializeField] TextMeshProUGUI collectTex;
+
     private QuizSO realSo;
+
+    bool isRoad = false;
     public void QuizStart()
     {
+        quizGroup.SetActive(true);
         Time.timeScale = 0.0f;
-        realSo = quizSOs[Random.Range(0, quizSOs.Count)];
-        quizTexs[0].text = realSo._quizTex;
-        for (int i = 1; i < quizTexs.Count; i++)
-        {
-            quizTexs[i].text = realSo._quizTexList[i-1];
-        }
+        QuizMake();
     }
     public void WhatIsCollect(int answer)
     {
         if(answer == realSo._collectNum)
         {
+            MessageUp(collectTex, true);
+        }
+        else
+        {
+            MessageUp(wrongTex, false);
+            QuizMake();
+        }
+    }
 
+    private void MessageUp(TextMeshProUGUI tex , bool isCollect)
+    {
+        if (isRoad)
+            return;
+
+        isRoad = true;
+        
+        Sequence seq = DOTween.Sequence();
+
+        tex.gameObject.SetActive(true);
+        Time.timeScale = 1.0f;
+        seq.Append(tex.DOFade(1, 0.2f));
+        seq.AppendInterval(0.5f);
+        seq.Append(tex.DOFade(0, 0.2f).OnComplete(() =>
+        {
+            if(isCollect)
+                quizGroup.SetActive(false);
+            tex.gameObject.SetActive(false);
+            isRoad = false;
+        }));
+
+    }
+    private void QuizMake()
+    {
+        if (isRoad)
+            return;
+
+        realSo = quizSOs[Random.Range(0, quizSOs.Count)];
+        quizTexs[0].text = realSo._quizTex;
+        for (int i = 1; i < quizTexs.Count; i++)
+        {
+            quizTexs[i].text = realSo._quizTexList[i - 1];
         }
     }
 }
